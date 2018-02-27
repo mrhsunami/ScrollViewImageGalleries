4806c559a3e75abbfa9f9ec0d47b61a7233bc757
Nathan <nathan.hsu@icloud.com>
2018-02-26 20:37:15 -0800

    Image detail view

---
+++ ScrollViewImageGalleries/ImageDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageDetailViewController.h
+//  ScrollViewImageGalleries
+//
+//  Created by Nathan Hsu on 2018-02-26.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ImageDetailViewController : ViewController <UIScrollViewDelegate>
+
+@end
---
+++ ScrollViewImageGalleries/ImageDetailViewController.m
@@ -0,0 +1,53 @@
+//
+//  ImageDetailViewController.m
+//  ScrollViewImageGalleries
+//
+//  Created by Nathan Hsu on 2018-02-26.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+#import "ImageDetailViewController.h"
+
+@interface ImageDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UIScrollView *imageDetailScrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+
+
+@implementation ImageDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.imageDetailScrollView.delegate = self;
+    self.imageDetailScrollView.minimumZoomScale = 0.1;
+    self.imageDetailScrollView.maximumZoomScale = 1.0;
+    
+    
+    
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+- (nullable UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
